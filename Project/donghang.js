171fff4826c55ed79b6815a4b22d9a00380c458a
zprime0920 <zprime0920@gmail.com>
2017-05-18 00:38:39 +0900

    main page sketch

--- Project/donghang.js
+++ Project/donghang.js
@@ -1,21 +1,3 @@
-// $(function() {
-//     var output = $('#output');
-
-//     function handleOrientation(event) {
-//         var x = event.beta;  // In degree in the range [-180,180]
-//         var y = event.gamma; // In degree in the range [-90,90]
-//         var z = event.alpha;
-
-//         var text = "";
-//         text += "alpha: " + z + "\n";
-//         text += "beta: " + x + "\n";
-//         text += "gamma: " + y + "\n";
-//         output.text(text);
-//     }
-
-//     window.addEventListener('deviceorientation', handleOrientation);
-// });
-
 function activateOverlay() {
   // show overlay
   mui.overlay('on');
