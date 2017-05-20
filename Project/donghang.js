ba1d86bb3071f1be1a64b45632b6a3e0d380ab2c
zprime0920 <zprime0920@gmail.com>
2017-05-21 03:41:16 +0900

    Adjust zooming action of upload button

--- Project/donghang.js
+++ Project/donghang.js
@@ -13,13 +13,25 @@ $(document).on("click", "#upload", function() {
 function growUpload() {
   height = 80;
   setInterval(function() {
-    var height = $("#upload-btn").css("height")
-    height = height.replace('px', '')
-    num = Number(height) + 3;
-    if (num < 93) {
-      height = num.toString()
-    $("#upload-btn").css("height", height+"px")
+    var width = $("#upload-btn").css("width");
+    var height = $("#upload-btn").css("height");
+    var other_width = $("#photo-btn").css("width");
+  
+    width = width.replace('px', '');
+    height = height.replace('px', '');
+    other_width = other_width.replace('px', '');
+    heigth_num = Number(height) + 10;
+    width_num = Number(width) + 4;
+    num = Number(other_width) - 2;
+    if (heigth_num < 120) {
+      height = heigth_num.toString();
+      width = width_num.toString();
+      other_width = num.toString();
+    $("#upload-btn").css("width", width+"px");
+    $("#upload-btn").css("height", height+"px");
+    $("#photo-btn").css("width", other_width+"px");
+    $("#message-btn").css("width", other_width+"px");
     }
-  }, 200);
+  }, 100);
 }
 setTimeout(growUpload, 5000);
